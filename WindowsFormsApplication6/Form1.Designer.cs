f8b37e3157c28ddec9b23bb6e34d3400e178dfb9
Ralphive <ralphrpo@gmail.com>
2018-11-15 14:26:08 +0000

    initial commit

---
+++ WindowsFormsApplication6/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace WindowsFormsApplication6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DIS_Connect = new System.Windows.Forms.Button();
+            this.DIS_CreateBP = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.DIS_CardType = new System.Windows.Forms.ComboBox();
+            this.DIS_GetBpList = new System.Windows.Forms.Button();
+            this.DIS_BPCode = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.DIA_CardType = new System.Windows.Forms.ComboBox();
+            this.DIA_GetBpList = new System.Windows.Forms.Button();
+            this.DIA_BPCode = new System.Windows.Forms.TextBox();
+            this.DIA_Connect = new System.Windows.Forms.Button();
+            this.DIA_CreateBP = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // DIS_Connect
+            // 
+            this.DIS_Connect.Location = new System.Drawing.Point(6, 19);
+            this.DIS_Connect.Name = "DIS_Connect";
+            this.DIS_Connect.Size = new System.Drawing.Size(178, 23);
+            this.DIS_Connect.TabIndex = 0;
+            this.DIS_Connect.Text = "Connect";
+            this.DIS_Connect.UseVisualStyleBackColor = true;
+            this.DIS_Connect.Click += new System.EventHandler(this.DISConnect_Click);
+            // 
+            // DIS_CreateBP
+            // 
+            this.DIS_CreateBP.Location = new System.Drawing.Point(6, 73);
+            this.DIS_CreateBP.Name = "DIS_CreateBP";
+            this.DIS_CreateBP.Size = new System.Drawing.Size(75, 20);
+            this.DIS_CreateBP.TabIndex = 1;
+            this.DIS_CreateBP.Text = "Create BP";
+            this.DIS_CreateBP.UseVisualStyleBackColor = true;
+            this.DIS_CreateBP.Click += new System.EventHandler(this.DIS_CreateBP_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.DIS_CardType);
+            this.groupBox1.Controls.Add(this.DIS_GetBpList);
+            this.groupBox1.Controls.Add(this.DIS_BPCode);
+            this.groupBox1.Controls.Add(this.DIS_Connect);
+            this.groupBox1.Controls.Add(this.DIS_CreateBP);
+            this.groupBox1.Location = new System.Drawing.Point(12, 20);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(192, 102);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "DI Server";
+            // 
+            // DIS_CardType
+            // 
+            this.DIS_CardType.FormattingEnabled = true;
+            this.DIS_CardType.Items.AddRange(new object[] {
+            "Lid",
+            "Customer",
+            "Supplier"});
+            this.DIS_CardType.Location = new System.Drawing.Point(84, 50);
+            this.DIS_CardType.Name = "DIS_CardType";
+            this.DIS_CardType.Size = new System.Drawing.Size(97, 21);
+            this.DIS_CardType.TabIndex = 4;
+            this.DIS_CardType.SelectedIndexChanged += new System.EventHandler(this.DIS_CardType_SelectedIndexChanged);
+            // 
+            // DIS_GetBpList
+            // 
+            this.DIS_GetBpList.Location = new System.Drawing.Point(6, 50);
+            this.DIS_GetBpList.Name = "DIS_GetBpList";
+            this.DIS_GetBpList.Size = new System.Drawing.Size(75, 21);
+            this.DIS_GetBpList.TabIndex = 3;
+            this.DIS_GetBpList.Text = "Get BP List";
+            this.DIS_GetBpList.UseVisualStyleBackColor = true;
+            this.DIS_GetBpList.Click += new System.EventHandler(this.DIS_GetBpList_Click);
+            // 
+            // DIS_BPCode
+            // 
+            this.DIS_BPCode.Location = new System.Drawing.Point(84, 73);
+            this.DIS_BPCode.Name = "DIS_BPCode";
+            this.DIS_BPCode.Size = new System.Drawing.Size(97, 20);
+            this.DIS_BPCode.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.DIA_CardType);
+            this.groupBox2.Controls.Add(this.DIA_GetBpList);
+            this.groupBox2.Controls.Add(this.DIA_BPCode);
+            this.groupBox2.Controls.Add(this.DIA_Connect);
+            this.groupBox2.Controls.Add(this.DIA_CreateBP);
+            this.groupBox2.Location = new System.Drawing.Point(240, 20);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(198, 102);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "DI API";
+            // 
+            // DIA_CardType
+            // 
+            this.DIA_CardType.FormattingEnabled = true;
+            this.DIA_CardType.Items.AddRange(new object[] {
+            "Lid",
+            "Customer",
+            "Supplier"});
+            this.DIA_CardType.Location = new System.Drawing.Point(84, 50);
+            this.DIA_CardType.Name = "DIA_CardType";
+            this.DIA_CardType.Size = new System.Drawing.Size(97, 21);
+            this.DIA_CardType.TabIndex = 4;
+            // 
+            // DIA_GetBpList
+            // 
+            this.DIA_GetBpList.Location = new System.Drawing.Point(6, 50);
+            this.DIA_GetBpList.Name = "DIA_GetBpList";
+            this.DIA_GetBpList.Size = new System.Drawing.Size(75, 21);
+            this.DIA_GetBpList.TabIndex = 3;
+            this.DIA_GetBpList.Text = "Get BP List";
+            this.DIA_GetBpList.UseVisualStyleBackColor = true;
+            this.DIA_GetBpList.Click += new System.EventHandler(this.DIA_GetBpList_Click);
+            // 
+            // DIA_BPCode
+            // 
+            this.DIA_BPCode.Location = new System.Drawing.Point(84, 73);
+            this.DIA_BPCode.Name = "DIA_BPCode";
+            this.DIA_BPCode.Size = new System.Drawing.Size(97, 20);
+            this.DIA_BPCode.TabIndex = 2;
+            // 
+            // DIA_Connect
+            // 
+            this.DIA_Connect.Location = new System.Drawing.Point(6, 19);
+            this.DIA_Connect.Name = "DIA_Connect";
+            this.DIA_Connect.Size = new System.Drawing.Size(178, 23);
+            this.DIA_Connect.TabIndex = 0;
+            this.DIA_Connect.Text = "Connect";
+            this.DIA_Connect.UseVisualStyleBackColor = true;
+            this.DIA_Connect.Click += new System.EventHandler(this.DIA_Connect_Click);
+            // 
+            // DIA_CreateBP
+            // 
+            this.DIA_CreateBP.Location = new System.Drawing.Point(6, 73);
+            this.DIA_CreateBP.Name = "DIA_CreateBP";
+            this.DIA_CreateBP.Size = new System.Drawing.Size(75, 20);
+            this.DIA_CreateBP.TabIndex = 1;
+            this.DIA_CreateBP.Text = "Create BP";
+            this.DIA_CreateBP.UseVisualStyleBackColor = true;
+            this.DIA_CreateBP.Click += new System.EventHandler(this.DIA_CreateBP_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 131);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "DI Server x DIAPI - Sample LSRetail";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button DIS_Connect;
+        private System.Windows.Forms.Button DIS_CreateBP;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox DIS_BPCode;
+        private System.Windows.Forms.Button DIS_GetBpList;
+        private System.Windows.Forms.ComboBox DIS_CardType;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox DIA_CardType;
+        private System.Windows.Forms.Button DIA_GetBpList;
+        private System.Windows.Forms.TextBox DIA_BPCode;
+        private System.Windows.Forms.Button DIA_Connect;
+        private System.Windows.Forms.Button DIA_CreateBP;
+    }
+}
+
---
+++ WindowsFormsApplication6/Form1.cs
@@ -0,0 +1,447 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication6
+{
+    public partial class Form1 : Form
+    {
+        private const string sRem = "|Empty Node|";
+
+        static String DataBaseServer = "ralpholiveie7eb";
+        static String DataBaseName = "SBODEMOUS";
+        static String DataBaseType = "dst_MSSQL2012";
+        static String DataBaseUserName = "sa";
+        static String DataBasePassword = "sapb1";
+        static String CompanyUserName = "manager";
+        static String CompanyPassword = "1234";
+        static String Language = "ln_English";
+        static String LicenseServer = DataBaseServer + ":30000";
+        static String DIS_SessionId = null;
+
+
+        //DI API
+        private SAPbobsCOM.Company objCompany = null;
+        private int intRetCode = -1;
+        private string strMessage = "";
+        private SAPbobsCOM.BusinessPartners objBP = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void DISConnect_Click(object sender, EventArgs e)
+        {
+
+            SBODI_Server.Node DISnode = null;
+            string sSOAPans = null, sCmd = null;
+
+            DISnode = new SBODI_Server.Node();
+
+            sCmd = @"<?xml version=""1.0"" encoding=""UTF-16""?>"
+                    + @"<env:Envelope xmlns:env=""http://schemas.xmlsoap.org/soap/envelope/"">"
+                        + @"<env:Body>"
+                                + @"<dis:Login xmlns:dis=""http://www.sap.com/SBO/DIS"">"
+                                    + "<DatabaseServer>" + DataBaseServer + "</DatabaseServer>"
+                                    + "<DatabaseName>" + DataBaseName + "</DatabaseName>"
+                                    + "<DatabaseType>" + DataBaseType + "</DatabaseType>"
+                                    + "<DatabaseUsername>" + DataBaseUserName + "</DatabaseUsername>"
+                                    + "<DatabasePassword>" + DataBasePassword + "</DatabasePassword>"
+                                    + "<CompanyUsername>" + CompanyUserName + "</CompanyUsername>"
+                                    + "<CompanyPassword>" + CompanyPassword + "</CompanyPassword>"
+                                    + "<Language>" + Language + "</Language>"
+                                    + "<LicenseServer>" + LicenseServer + "</LicenseServer>"
+                              + "</dis:Login>"
+                        + "</env:Body>"
+                    + "</env:Envelope>";
+
+            sSOAPans = DISnode.Interact(sCmd);
+
+            //  Parse the SOAP answer
+            System.Xml.XmlValidatingReader xmlValid = null;
+            string sRet = null;
+            xmlValid = new System.Xml.XmlValidatingReader(sSOAPans, System.Xml.XmlNodeType.Document, null);
+            while (xmlValid.Read())
+            {
+                if (xmlValid.NodeType == System.Xml.XmlNodeType.Text)
+                {
+                    if (sRet == null)
+                    {
+                        sRet += xmlValid.Value;
+                    }
+                    else
+                    {
+                        if (sRet.StartsWith("Error"))
+                        {
+                            sRet += " " + xmlValid.Value;
+                        }
+                        else
+                        {
+                            sRet = "Error " + sRet + " " + xmlValid.Value;
+                        }
+                    }
+                }
+            }
+
+            if (sSOAPans.Contains("<env:Fault>"))
+            {
+                sRet = "Error: " + sRet;
+            }
+            else
+            {
+                DIS_SessionId = sRet;
+                sRet = "Connection ID: " + DIS_SessionId;
+            }
+
+            MessageBox.Show(sRet);
+        }
+
+        private void DIS_CreateBP_Click(object sender, EventArgs e)
+        {
+
+
+            //Formats XML with BP attributes
+            System.Xml.XmlDocument xmlD = null;
+            System.Xml.XmlElement xmlE = null;
+            System.Xml.XmlNodeList bpCode = null;
+            System.Xml.XmlNode bpName = null;
+
+
+            xmlD = GetEmptyBPXml();
+            xmlE = (System.Xml.XmlElement)xmlD.DocumentElement;
+
+            xmlE.InnerXml = xmlE.InnerXml.Replace("<CardCode />", "<CardCode>" + DIS_BPCode.Text + "</CardCode>");
+            xmlE.InnerXml = xmlE.InnerXml.Replace("<CardName />", "<CardName>" + "Inserted By DI Server at - " + DIS_BPCode.Text + "</CardName>");
+
+            SBODI_Server.Node node = null;
+            System.Xml.XmlDocument doc = null;
+            System.Xml.XmlDocument parseXML = null;
+
+            doc = new System.Xml.XmlDocument();
+            node = new SBODI_Server.Node();
+
+            string AddCmd = null;
+            System.Xml.XmlNode netoXML = null;
+
+            parseXML = new System.Xml.XmlDocument();
+            parseXML.LoadXml(xmlE.OuterXml);
+
+            netoXML = (RemoveEmptyNodes(parseXML));
+
+            AddCmd = @"<?xml version=""1.0"" encoding=""UTF-16""?>" +
+                        @"<env:Envelope xmlns:env=""http://schemas.xmlsoap.org/soap/envelope/"">" +
+                            "<env:Header>" +
+                                "<SessionID>" + DIS_SessionId + "</SessionID>" +
+                            @"</env:Header>" +
+                            "<env:Body>" +
+                                @"<dis:AddObject xmlns:dis=""http://www.sap.com/SBO/DIS"">" +
+                                    netoXML.InnerXml +
+                                "</dis:AddObject>" +
+                            "</env:Body>" +
+                        "</env:Envelope>";
+
+            string res = null;
+            res = node.Interact(AddCmd);
+            doc.LoadXml(res);
+            showXmlReturn(doc);
+
+        }
+
+        private void DIS_GetBpList_Click(object sender, EventArgs e)
+        {
+            SBODI_Server.Node DISnode = null;
+            string strSOAPans = null, strSOAPcmd = null;
+            System.Xml.XmlDocument xmlDoc = null;
+
+            xmlDoc = new System.Xml.XmlDocument();
+            DISnode = new SBODI_Server.Node();
+
+            strSOAPcmd = @"<?xml version=""1.0"" encoding=""UTF-16""?>" +
+                            @"<env:Envelope xmlns:env=""http://schemas.xmlsoap.org/soap/envelope/"">" +
+                                "<env:Header>" +
+                                    "<SessionID>" + DIS_SessionId + "</SessionID>" +
+                                @"</env:Header>" +
+                                "<env:Body>" +
+                                    @"<dis:GetBPList xmlns:dis=""http://www.sap.com/SBO/DIS"">" +
+                                        "<CardType>" + getCardtype(DIS_CardType.Text) + "</CardType>" +
+                                    "</dis:GetBPList>" +
+                                "</env:Body>" +
+                            "</env:Envelope>";
+
+            strSOAPans = DISnode.Interact(strSOAPcmd);
+            xmlDoc.LoadXml(strSOAPans);
+            showXmlReturn(xmlDoc);    
+        }
+
+
+        private bool DISConnected()
+        {
+            if (DIS_SessionId == null)
+                return false;
+            return true;
+        }
+
+
+
+        // This function returns an XML document of an empty Business Partner object
+        // To do so, it calls DI Server asking the object BusinessPartners
+        public System.Xml.XmlDocument GetEmptyBPXml()
+        {
+
+            SBODI_Server.Node n = null;
+            string s = null, strXML = null;
+            System.Xml.XmlDocument d = null;
+
+            d = new System.Xml.XmlDocument();
+            n = new SBODI_Server.Node();
+
+            strXML = @"<?xml version=""1.0"" encoding=""UTF-16""?>" +
+                     @"<env:Envelope xmlns:env=""http://schemas.xmlsoap.org/soap/envelope/"">" +
+                        "<env:Header>" +
+                            "<SessionID>" + DIS_SessionId + "</SessionID>"
+                        + @"</env:Header>" +
+                        "<env:Body>" +
+                            @"<dis:GetBusinessObjectTemplate xmlns:dis=""http://www.sap.com/SBO/DIS"">" +
+                                "<Object>oBusinessPartners</Object>" +
+                            "</dis:GetBusinessObjectTemplate>" +
+                        " </env:Body>" +
+                     "</env:Envelope>";
+
+            s = n.Interact(strXML);
+            d.LoadXml(s);
+
+            return (RemoveEnv(d));
+        }
+
+        //  This function removes the SOAP envelope
+        public System.Xml.XmlDocument RemoveEnv(System.Xml.XmlDocument xmlD)
+        {
+            System.Xml.XmlDocument d = null;
+            string s = null;
+
+            d = new System.Xml.XmlDocument();
+            if (xmlD.InnerXml.Contains("<env:Fault>"))
+            {
+                return xmlD;
+            }
+            else
+            {
+                s = xmlD.FirstChild.NextSibling.FirstChild.FirstChild.InnerXml;
+                d.LoadXml(s);
+            }
+
+            return d;
+
+        }
+        //  This function removes all the empty nodes from an XML document
+        private System.Xml.XmlNode RemoveEmptyNodes(System.Xml.XmlNode n)
+        {
+            System.Xml.XmlNode nAns = null;
+
+            nAns = MarkEmptyNodes(n);
+            System.Xml.XmlNodeList nc = null;
+            string sSelect = null;
+
+            sSelect = @"//*[text()=""";
+            sSelect += sRem;
+            sSelect += @"""]";
+
+            nc = nAns.SelectNodes(sSelect);
+            foreach (System.Xml.XmlNode nN in nc)
+            {
+                nN.ParentNode.RemoveChild(nN);
+            }
+            return nAns;
+        }
+        //  This function marks all the empty nodes with special text.
+        //  The "RemoveEmptyNodes" function uses it to select the nodes to be deleted.
+        private System.Xml.XmlNode MarkEmptyNodes(System.Xml.XmlNode n)
+        {
+            System.Xml.XmlNode MainNode = null;
+            MainNode = n;
+            System.Xml.XmlNode nI = null;
+
+            int i = 0, Removed = 0;
+            i = 0;
+            Removed = 0;
+
+            for (i = 0; i <= MainNode.ChildNodes.Count - 1 - Removed; i++)
+            {
+                nI = MainNode.ChildNodes[i];
+                if (nI.InnerText == "")
+                {
+                    nI.InnerText = sRem;
+                }
+                else if (nI.HasChildNodes)
+                {
+                    nI = MarkEmptyNodes(nI);
+                }
+            }
+            return MainNode;
+        }
+
+
+
+        private String getCardtype(String desc)
+        {
+            return "c"+desc;
+        }
+        private SAPbobsCOM.BoCardTypes getDIACardtype(String desc)
+        {
+            if (desc == "Supplier"){
+                return SAPbobsCOM.BoCardTypes.cSupplier;
+            }
+            else if (desc == "Customer")
+            {
+                return SAPbobsCOM.BoCardTypes.cCustomer;
+            }
+            return SAPbobsCOM.BoCardTypes.cLid;
+ 
+
+        }
+
+        private void showXmlReturn(System.Xml.XmlDocument doc)
+        {
+            MessageBox.Show("XML Return: \n" + doc.InnerXml);
+        }
+
+        private void DIS_CardType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DIA_Connect_Click(object sender, EventArgs e)
+        {
+            this.connect();
+
+        }
+
+        private void DIA_GetBpList_Click(object sender, EventArgs e)
+        {
+            SAPbobsCOM.Recordset objRecordset = null;
+            try
+            {
+                if (this.objCompany == null || !objCompany.Connected)
+                {
+                    this.connect();
+                }
+                if (this.objCompany != null && objCompany.Connected)
+                {
+                    objBP = (SAPbobsCOM.BusinessPartners)objCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.oBusinessPartners);
+                    objRecordset = (SAPbobsCOM.Recordset)objCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset);
+
+                    SAPbobsCOM.SBObob objBOB = null;
+                    objBOB = objCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoBridge);
+                    objRecordset = objBOB.GetBPList(getDIACardtype(DIA_CardType.Text));
+                    objBP.Browser.Recordset = objRecordset;
+
+                    objBP.Browser.MoveFirst();
+                    
+                    String msg = null;
+                    while (!objBP.Browser.EoF)
+                    {
+                        msg += objBP.CardCode + " - " + objBP.CardName + "\n";
+                        objBP.Browser.MoveNext();
+                    }
+
+                    MessageBox.Show(msg);
+                        
+                }
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show(er.Message);
+            }
+        }
+
+        private void DIA_CreateBP_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (this.objCompany == null || !objCompany.Connected)
+                {
+                    this.connect();
+                }
+                if (this.objCompany != null && objCompany.Connected)
+                {
+
+                    SAPbobsCOM.BusinessPartners oBP = null;
+                    int retCode = -1;
+                    String retMess = "";
+
+                    oBP = objCompany.GetBusinessObject(
+                                    SAPbobsCOM.BoObjectTypes.oBusinessPartners);
+
+                    oBP.CardCode = DIA_BPCode.Text;
+                    oBP.CardName = "Insert via DIAPI - "+ DIA_BPCode.Text;
+                    oBP.CardType = SAPbobsCOM.BoCardTypes.cCustomer;
+                    retCode = oBP.Add();
+
+                    if (retCode != 0)
+                    {
+                        retMess = objCompany.GetLastErrorDescription();
+                    }
+                    else
+                    {
+                        retMess = "BP inserted successfully! - " + objCompany.GetNewObjectKey();
+                    }
+
+                    MessageBox.Show(retMess);
+                }
+
+            }
+            catch (Exception er)
+            {
+
+                MessageBox.Show(er.Message);
+            }
+
+        }
+
+        private void connect()
+        {
+            try
+            {
+                objCompany = new SAPbobsCOM.Company();
+                objCompany.Server = DataBaseServer;
+                objCompany.UseTrusted = false;
+                objCompany.DbUserName = DataBaseUserName;
+                objCompany.DbPassword = DataBasePassword;
+                objCompany.DbServerType = SAPbobsCOM.BoDataServerTypes.dst_MSSQL2012;
+                objCompany.CompanyDB = DataBaseName;
+                objCompany.UserName = CompanyUserName;
+                objCompany.Password = CompanyPassword;
+
+                intRetCode = objCompany.Connect();
+
+                if (intRetCode != 0)
+                {
+                    strMessage = objCompany.GetLastErrorDescription();
+                }
+                else
+                {
+                    strMessage = "Connected to " + objCompany.CompanyName;
+                }
+                MessageBox.Show(strMessage);
+
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show(er.Message);
+            }
+        }
+
+
+
+
+    }
+}
